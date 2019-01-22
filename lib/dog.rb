a91b1137e4288281ce633668165462a1b54b06e1
Trent Moss <trentmoss05@gmail.com>
2019-01-22 15:39:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+#writes the dogs name
+  def name=(dog_name)
+    @name = dog_name
+  end
+#reads the dog name
+  def name
+    @name
+  end
+  #writes the dogs breed
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  #reads the dogs breed
+  def breed
+    @breed
+  end
+
+end
